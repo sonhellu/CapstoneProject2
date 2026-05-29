0e16d6533cb20e11b522c6b8b43c550c73290fab
sonhellu <sonhellu186@gmail.com>
2026-05-30 01:12:46 +0900

    update

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, pool_size=10, max_overflow=20)
+engine = create_engine(settings.get_database_url(), pool_size=10, max_overflow=20)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
